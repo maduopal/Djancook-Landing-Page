6e8e8f176b95ceae41fe5adfe832073b24fd3ffd
maduopal <maduopal@gmail.com>
2025-09-16 14:45:50 +0700

    Initial landing page upload

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Landing page Djancook siap!");
+
+// CTA animasi tambahan
+document.querySelector(".cta-btn").addEventListener("mouseover", () => {
+    console.log("User hover CTA button");
+});
