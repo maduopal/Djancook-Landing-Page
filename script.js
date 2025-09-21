e0ead6f27d653c129a6edcb512555256d2d85be2
maduopal <maduopal@gmail.com>
2025-09-21 21:30:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 function hubungiAdmin() {
-window.open("https://wa.me/6281234567890", "_blank");
+  window.open("https://wa.me/6281234567890", "_blank");
 }
 
-
 function pesanSekarang() {
-window.open("https://wa.me/6281234567890", "_blank");
+  window.open("https://wa.me/6281234567890", "_blank");
 }
